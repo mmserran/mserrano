416e794c22d6e7fa9494089948d76344d9679d4e
ENTslug <Mark.Serrano26@gmail.com>
2018-04-25 20:13:17 -0700

    midtier: write PHP tests for input.

--- unittests/library/php/input.test.php
+++ unittests/library/php/input.test.php
@@ -2,16 +2,32 @@
 
 class inputTest extends UnitTestCase {
 
+    protected $rnd;
+
     function setUp() {
-        error_log(print_r('input test', true));
+        $this->rnd = random_int(0, 200);
     }
 
     function tearDown() {
         
     }
-    
+
+    function test___construct() {
+        $obj = new input($this->rnd + 0, $this->rnd + 1);
+        $res = is_a($obj, 'input') === true;
+        $this->assertTrue($res);
+
+        $res = $obj->data(input::get);
+        $this->assertNotNull($res);
+    }
+
     function test_data() {
-        $obj = new input('get', 'post');
+        $obj = new input($this->rnd + 0, $this->rnd + 1);
+        $res = $obj->data(input::get);
+        $this->assertEqual($this->rnd + 0, $res);
+
+        $res = $obj->data(input::post);
+        $this->assertEqual($this->rnd + 1, $res);
     }
 
 }
